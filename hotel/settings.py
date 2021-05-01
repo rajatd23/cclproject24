cfe62d9dfd2782d883c5dbd90d593cd3e3d8cf2d
rajatd23 <72095446+rajatd23@users.noreply.github.com>
2021-05-01 16:55:08 +0530

    Update settings.py

--- hotel/settings.py
+++ hotel/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-config.serve_static_assets = true
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
